d3497819d91d9684faf7ff49270f8801cd757bc6
Tamas Bihari <bhrimprove@gmail.com>
2025-06-02 21:17:40 +0200

    chore: remove unnecessary comment

--- crm-frontend/src/lib/utils/form-data-parser.ts
+++ crm-frontend/src/lib/utils/form-data-parser.ts
@@ -1,19 +1,3 @@
-// export function nestData<T>(flatData: Record<string, FormDataEntryValue>): T {
-//   const nested: T = {} as T;
-//   for (const [key, value] of Object.entries(flatData)) {
-//     const keys = key.split(".");
-//     keys.reduce((acc, k, i) => {
-//       if (i === keys.length - 1) {
-//         acc[k] = value;
-//       } else {
-//         acc[k] = acc[k] || {};
-//       }
-//       return acc[k];
-//     }, nested);
-//   }
-//   return nested;
-// }
-
 export function nestData<T>(flatData: Record<string, FormDataEntryValue>): T {
   const nested = {} as Record<string, unknown>;
   for (const [key, value] of Object.entries(flatData)) {
